8d22c31da63e09e2fe4b6e78a62bc8280f62be1f
max <max.lillie95@gmail.com>
2023-04-22 18:59:34 -0500

    controller for user created, included password_digest for auth

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,25 @@
+class UserController < ApplicationController
+  def first_name
+  end
+
+  def last_name
+  end
+
+  def middle_initial
+  end
+
+  def birth_year
+  end
+
+  def birth_month
+  end
+
+  def birth_day
+  end
+
+  def password_digest
+  end
+
+  def email
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'user/first_name'
+  get 'user/last_name'
+  get 'user/middle_initial'
+  get 'user/birth_year'
+  get 'user/birth_month'
+  get 'user/birth_day'
+  get 'user/password_digest'
+  get 'user/email'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
